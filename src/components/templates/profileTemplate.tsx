7a6353d59b7b291aa750263bf7081f884965febf
Yusuke Yamane <yusukey7grizi@gmail.com>
2022-01-25 01:48:37 +0900

    fixed design

--- src/components/templates/profileTemplate.tsx
+++ src/components/templates/profileTemplate.tsx
@@ -1,10 +1,10 @@
 import React, { FC, useContext } from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, useMediaQuery } from '@mui/material';
 import { MuiDivider } from 'components/atoms/divider';
 import { MovieListTitle } from 'components/atoms/texts';
-import { SearchedMovieList as MovieList } from 'components/organisms/searchedMovieList';
 import { AppContext } from 'contexts/appContext';
-import { FontSize } from 'components/constants';
+import { FontSize, MinScreenSize } from 'components/constants';
+import { MovieList } from 'components/organisms/movieList';
 
 type Props = {
   userName: string;
@@ -13,24 +13,36 @@ type Props = {
 
 const ProfileTemplate: FC<Props> = ({ userName, email }) => {
   const { relatedMovieList } = useContext(AppContext);
+  const isLargeScreenSize = useMediaQuery(MinScreenSize['s']);
+  const fontSize = isLargeScreenSize ? FontSize['m'] : FontSize['xs'];
 
   return (
-    <>
-      <Box sx={{ width: '50rem', margin: 'auto' }}>
-        <Typography
-          sx={{ pl: '8rem', fontWeight: 'bold' }}
-          fontSize={FontSize['xl']}
-        >
-          ユーザーネーム: {userName}
-        </Typography>
-        <Typography sx={{ pl: '9rem' }} fontSize={FontSize['m']}>
-          メールアドレス: {email}
-        </Typography>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      <Typography
+        sx={{ textAlign: 'center', fontWeight: 'bold', pb: '2rem' }}
+        fontSize={FontSize['xl']}
+      >
+        マイページ
+      </Typography>
+      <Box
+        sx={{
+          textAlign: 'left',
+          margin: 'auto',
+          pb: '7rem',
+        }}
+      >
+        <Typography fontSize={fontSize}>ユーザーネーム: {userName}</Typography>
+        <Typography fontSize={fontSize}>メールアドレス: {email}</Typography>
       </Box>
       <MovieListTitle />
       <MuiDivider />
       <MovieList movieList={relatedMovieList} />
-    </>
+    </Box>
   );
 };
 
