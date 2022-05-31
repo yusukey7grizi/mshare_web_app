37f15562c5e9e006ab979c0cc95cab9f743b2cbf
Hiroki <hiroki11hanada@gmail.com>
2022-05-31 17:31:02 +0900

    Fix: tweeks for serverless api

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -4,8 +4,6 @@ const axiosDefaultInstance = axios.create({
   baseURL:
     process.env.NEXT_PUBLIC_STAGE === 'dev'
       ? process.env.NEXT_PUBLIC_API_DOMAIN_DEV
-      : process.env.NEXT_PUBLIC_STAGE === 'sl'
-      ? process.env.NEXT_PUBLIC_API_DOMAIN_SL
       : process.env.NEXT_PUBLIC_API_DOMAIN_PROD,
 });
 
