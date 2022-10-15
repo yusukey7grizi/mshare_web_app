02c39a5469c4b0d97aa3ffd1f1ba8e60c7aa8674
Yusuke Yamane <yusukey7grizi@gmail.com>
2022-10-16 00:39:03 +0900

    fix: Modify the styles in the profile template page

--- src/components/templates/profileTemplate.tsx
+++ src/components/templates/profileTemplate.tsx
@@ -1,8 +1,8 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import { Box, Typography, useMediaQuery } from '@mui/material';
 import { MuiDivider } from 'components/atoms/divider';
 import { MovieListTitle } from 'components/atoms/texts';
-import { FontSize, ScreenSize } from 'components/constants';
+import { BasePixel, FontSize, ScreenSize } from 'components/constants';
 import { MovieList } from 'components/organisms/movieList';
 import { Movie } from 'types/dataTypes';
 import { Bar } from 'components/organisms';
@@ -17,37 +17,57 @@ const ProfileTemplate: FC<Props> = ({ username, email, movieList }) => {
   const isLargerThanIphone = useMediaQuery(ScreenSize.largerThanIphone);
   const fontSize = isLargerThanIphone ? FontSize['m'] : FontSize['xs'];
 
+  const usernameText = useMemo(() => {
+    return `ユーザーネーム: ${username}`;
+  }, [username]);
+
+  const emailText = useMemo(() => {
+    return `メールアドレス: ${email}`;
+  }, [email]);
+
+  const styles = {
+    outermostBox: {
+      display: 'flex',
+      flexDirection: 'column',
+      marginLeft: BasePixel * 6,
+      marginRight: BasePixel * 6,
+      marginTop: BasePixel * 26,
+      gap: BasePixel * 10,
+    },
+    title: {
+      textAlign: 'center',
+      fontWeight: 'bold',
+      fontSize: FontSize['xl'],
+    },
+    userInfoBox: {
+      textAlign: 'left',
+      margin: 'auto',
+    },
+    userInfo: {
+      fontSize: fontSize,
+    },
+  } as const;
+
   return (
-    <Bar>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
-        <Typography
-          sx={{ textAlign: 'center', fontWeight: 'bold', pb: '2rem' }}
-          fontSize={FontSize['xl']}
-        >
+    <>
+      <Bar />
+      <Box sx={styles.outermostBox}>
+        <Typography sx={styles.title} fontSize={FontSize['xl']} color='primary'>
           マイページ
         </Typography>
-        <Box
-          sx={{
-            textAlign: 'left',
-            margin: 'auto',
-            pb: '7rem',
-          }}
-        >
-          <Typography fontSize={fontSize}>
-            ユーザーネーム: {username}
-          </Typography>
-          <Typography fontSize={fontSize}>メールアドレス: {email}</Typography>
+        <Box sx={styles.userInfoBox}>
+          <Typography sx={styles.userInfo}>{usernameText}</Typography>
+          <Typography sx={styles.userInfo}>{emailText}</Typography>
         </Box>
-        <MovieListTitle />
-        <MuiDivider />
-        <MovieList movieList={movieList} />
+        {movieList.length > 0 && (
+          <>
+            <MovieListTitle />
+            <MuiDivider mb={BasePixel * 5} mt={BasePixel * 5} />
+            <MovieList movieList={movieList} />
+          </>
+        )}
       </Box>
-    </Bar>
+    </>
   );
 };
 
